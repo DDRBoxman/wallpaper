0a96b27791937c82ed0159f00bcb78d0deed98a7
Colin Edwards <Colin Edwards>
2016-06-04 16:35:09 -0700

    Setting wallpaper on windows works

--- wallpaper_windows.go
+++ wallpaper_windows.go
@@ -4,18 +4,10 @@ import (
 	"fmt"
 	"syscall"
 	"unsafe"
+
+	"github.com/lxn/win"
 )
 
 func Set(name string) {
-	var mod = syscall.NewLazyDLL("user32.dll")
-	var proc = mod.NewProc("SystemParametersInfoW")
-
-	var SPI_SETDESKWALLPAPER = 0x0014
-
-	ret, _, _ := proc.Call(0,
-		uintptr(SPI_SETDESKWALLPAPER),
-		uintptr(0),
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(name))),
-		uintptr(0))
-	fmt.Printf("Return: %d\n", ret)
+	win.SystemParametersInfo(20, 0, unsafe.Pointer(syscall.StringToUTF16Ptr(name)), 0)
 }
